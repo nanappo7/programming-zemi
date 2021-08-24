9e9087e09670558170794e4e91653b7b069f5480
nanappo7 <nana.momo.kazue.7th@gmail.com>
2021-08-24 11:50:52 +0000

    login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get 'contact' => "static_pages#contact"
   get 'signup' => "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destoroy'
   resources :users
 end
